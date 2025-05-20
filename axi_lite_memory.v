9d95e92ca0700ca16c722ae504526952c9e75b4e
Devanshu Gaur <113101862+Griseoh@users.noreply.github.com>
2025-05-20 23:47:02 +0530

    Update axi_lite_memory.v

--- axi_lite_memory.v
+++ axi_lite_memory.v
@@ -37,7 +37,7 @@ module axi_lite_memory #(parameter DW = 32)
     output reg              S_AXIL_WREADY,
 
     // Write Response Channel
-    output reg  [1:0]       S_AXIL_BRESP,
+    output      [1:0]       S_AXIL_BRESP,
     output reg              S_AXIL_BVALID,
     input                   S_AXIL_BREADY,
 
@@ -48,19 +48,20 @@ module axi_lite_memory #(parameter DW = 32)
 
     // Read Data Channel
     output reg  [DW-1:0]    S_AXIL_RDATA,
-    output reg  [1:0]       S_AXIL_RRESP,
+    output      [1:0]       S_AXIL_RRESP,
     output reg              S_AXIL_RVALID,
     input                   S_AXIL_RREADY 
   );
   
     integer i;  
+    reg [DW-1:0] check_data;
     
     // 4 x 32 x 64 bit Memory Blocks 
     reg [DW-1:0] block0 [0:63];
     reg [DW-1:0] block1 [0:63];
     reg [DW-1:0] block2 [0:63];
     reg [DW-1:0] block3 [0:63];
-    
+
     // Initializing Memory Blocks
     initial begin
         for(i = 0; i <64; i = i + 1)begin
@@ -73,6 +74,19 @@ module axi_lite_memory #(parameter DW = 32)
            
     // Handshake Flags
     reg aw_handshake, w_handshake, ar_handshake;
+    
+    // Response Flags
+    reg [1:0] b_resp, r_resp;
+    
+    // Write Response States
+    localparam WIDLE = 2'b00, WRITE = 2'b01, WCOMPUTE = 2'b10, WRESPOND = 2'b11;
+    
+    // Read Response States
+    localparam RIDLE = 2'b00, READ = 2'b01, RCOMPUTE = 2'b10, RRESPOND = 2'b11;
+    
+    // State Machine Registers
+    reg [1:0] w_c_state, w_n_state;
+    reg [1:0] r_c_state, r_n_state;
     
     // Channel Registers
     reg [31:0] write_addr;
@@ -122,15 +136,46 @@ module axi_lite_memory #(parameter DW = 32)
         end
     end
     
-    // Write Response Channel Logic 
+    // Write Response Channel Logic
     always @(posedge ACLK, negedge ARESETN)begin
         if(!ARESETN)begin
-            S_AXIL_BRESP <= 2'b00;
-            S_AXIL_BVALID <= 0;           
+            w_c_state <= WIDLE;
+            w_n_state <= WIDLE;
         end
         else begin
-            if(w_handshake && aw_handshake && !S_AXIL_BVALID)begin
-                case(write_addr[7:6])
+            w_c_state <= w_n_state;
+        end
+    end
+    always @(*)begin
+        case(w_c_state)
+            WIDLE : begin
+                if(w_handshake && aw_handshake && !S_AXIL_BVALID)begin
+                    w_n_state = WRITE;
+                end
+                else begin
+                    w_n_state = WIDLE;
+                end
+            end
+            WRITE : begin
+                w_n_state = WCOMPUTE;
+            end
+            WCOMPUTE : begin
+                w_n_state = WRESPOND;
+            end
+            WRESPOND : begin
+                if(S_AXIL_BVALID && S_AXIL_BREADY)begin
+                    w_n_state = WIDLE;
+                end
+                else begin
+                    w_n_state = WRESPOND;
+                end
+            end
+        endcase
+    end
+    always @(posedge ACLK)begin
+        if(w_c_state == WRITE)begin
+            check_data <= write_data;
+            case(write_addr[7:6])
                     2'b00 : begin
                         for (i = 0; i < 4; i = i + 1)
                             if (write_strb[i]) block0[write_addr[5:0]][i*8 +: 8] <= write_data[i*8 +: 8];
@@ -147,16 +192,75 @@ module axi_lite_memory #(parameter DW = 32)
                         for (i = 0; i < 4; i = i + 1)
                             if (write_strb[i]) block3[write_addr[5:0]][i*8 +: 8] <= write_data[i*8 +: 8];
                     end
-                endcase
-                S_AXIL_BVALID <= 1;
-                S_AXIL_BRESP <= 2'b00;
-            end
-            if(S_AXIL_BVALID && S_AXIL_BREADY)begin
-                S_AXIL_BVALID <= 0;
-            end
+            endcase
+        end
+    end
+    always @(posedge ACLK)begin
+        if(w_c_state == WCOMPUTE)begin
+            case(write_addr[7:6])
+                    2'b00 : begin
+                        if(check_data == block0[write_addr[5:0]])begin
+                            b_resp <= 2'b00;
+                        end
+                        else begin
+                            b_resp <= 2'b10;
+                        end
+                    end
+                    2'b01 : begin
+                        if(check_data == block1[write_addr[5:0]])begin
+                            b_resp <= 2'b00;
+                        end
+                        else begin
+                            b_resp <= 2'b10;
+                        end
+                    end
+                    2'b10 : begin
+                        if(check_data == block2[write_addr[5:0]])begin
+                            b_resp <= 2'b00;
+                        end
+                        else begin
+                            b_resp <= 2'b10;
+                        end
+                    end
+                    2'b11 : begin
+                        if(check_data == block3[write_addr[5:0]])begin
+                            b_resp <= 2'b00;
+                        end
+                        else begin
+                            b_resp <= 2'b10;
+                        end
+                    end
+            endcase
         end
     end
     
+    assign S_AXIL_BRESP = b_resp;
+    
+    always @(posedge ACLK, negedge ARESETN)begin
+        if(!ARESETN)begin
+            S_AXIL_BVALID <= 0;
+            b_resp <= 2'b01;
+        end
+        else begin
+            case(w_c_state)
+                WIDLE : begin
+                   S_AXIL_BVALID <= 0;
+                   b_resp <= 2'b01; 
+                end
+                WRITE : begin
+                end
+                WCOMPUTE : begin
+                    S_AXIL_BVALID <= 1;
+                end
+                WRESPOND : begin
+                    if(S_AXIL_BVALID && S_AXIL_BREADY)begin
+                        S_AXIL_BVALID <= 0;
+                    end
+                end
+            endcase
+        end
+    end
+
     // Read Address Channel Logic
     always @(posedge ACLK, negedge ARESETN)begin
         if(!ARESETN)begin
@@ -176,27 +280,129 @@ module axi_lite_memory #(parameter DW = 32)
         end
     end
     
-    // Read Data Channel Logic
+    // Read Data and Response Channel Logic
+    always @(posedge ACLK, negedge ARESETN)begin
+        if(!ARESETN)begin
+            r_c_state <= RIDLE;
+            r_n_state <= RIDLE;
+        end
+        else begin
+            r_c_state <= r_n_state;
+        end
+    end
+    always @(*)begin
+        case(r_c_state)
+            RIDLE : begin
+                if(ar_handshake && !S_AXIL_RVALID)begin
+                    r_n_state = READ;
+                end
+                else begin
+                    r_n_state = RIDLE;
+                end
+            end
+            READ : begin
+                r_n_state = RCOMPUTE;
+            end
+            RCOMPUTE : begin
+                r_n_state = RRESPOND;
+            end
+            RRESPOND : begin
+                if(S_AXIL_RVALID && S_AXIL_RREADY)begin
+                    r_n_state = RIDLE;
+                end
+                else begin
+                    r_n_state = RRESPOND;
+                end
+            end
+        endcase
+    end
+    always @(posedge ACLK)begin
+        if(r_c_state == READ)begin
+            case (read_addr[7:6])
+                    2'b00 : begin
+                        S_AXIL_RDATA <= block0[read_addr[5:0]];
+                        check_data <= block0[read_addr[5:0]];
+                    end
+                    2'b01 : begin
+                        S_AXIL_RDATA <= block1[read_addr[5:0]];
+                        check_data <= block1[read_addr[5:0]];
+                    end
+                    2'b10 : begin
+                        S_AXIL_RDATA <= block2[read_addr[5:0]];
+                        check_data <= block2[read_addr[5:0]];
+                    end
+                    2'b11 : begin
+                        S_AXIL_RDATA <= block3[read_addr[5:0]];
+                        check_data <= block3[read_addr[5:0]];
+                    end
+            endcase
+        end
+    end
+    always @(posedge ACLK)begin
+        if(r_c_state == WCOMPUTE)begin
+            case(read_addr[7:6])
+                    2'b00 : begin
+                        if(check_data == S_AXIL_RDATA)begin
+                            r_resp <= 2'b00;
+                        end
+                        else begin
+                            r_resp <= 2'b10;
+                        end
+                    end
+                    2'b01 : begin
+                        if(check_data == S_AXIL_RDATA)begin
+                            r_resp <= 2'b00;
+                        end
+                        else begin
+                            r_resp <= 2'b10;
+                        end
+                    end
+                    2'b10 : begin
+                        if(check_data == S_AXIL_RDATA)begin
+                            r_resp <= 2'b00;
+                        end
+                        else begin
+                            r_resp <= 2'b10;
+                        end
+                    end
+                    2'b11 : begin
+                        if(check_data == S_AXIL_RDATA)begin
+                            r_resp <= 2'b00;
+                        end
+                        else begin
+                            b_resp <= 2'b10;
+                        end
+                    end
+            endcase
+        end
+    end
+    
+    assign S_AXIL_RRESP = r_resp;
+   
     always @(posedge ACLK, negedge ARESETN)begin
         if(!ARESETN)begin
             S_AXIL_RVALID <= 0;
-            S_AXIL_RRESP <= 2'b00;
-            S_AXIL_RDATA <= 32'h00000000;
+            S_AXIL_RDATA <= 0;
+            r_resp <= 2'b01;
         end
         else begin
-            if(ar_handshake && !S_AXIL_RVALID)begin
-                case (read_addr[7:6])
-                    2'b00: S_AXIL_RDATA <= block0[read_addr[5:0]];
-                    2'b01: S_AXIL_RDATA <= block1[read_addr[5:0]];
-                    2'b10: S_AXIL_RDATA <= block2[read_addr[5:0]];
-                    2'b11: S_AXIL_RDATA <= block3[read_addr[5:0]];
-                endcase
-                S_AXIL_RVALID <= 1;
-                S_AXIL_RRESP <= 2'b00;
-            end
-            if(S_AXIL_RVALID && S_AXIL_RREADY)begin
-                S_AXIL_RVALID <= 0;
-            end
+            case(r_c_state)
+                RIDLE : begin
+                   S_AXIL_RVALID <= 0;
+                   r_resp <= 2'b01; 
+                end
+                READ : begin
+                end
+                RCOMPUTE : begin
+                    S_AXIL_RVALID <= 1;
+                end
+                RRESPOND : begin
+                    if(S_AXIL_RVALID && S_AXIL_RREADY)begin
+                        S_AXIL_RVALID <= 0;
+                    end
+                end
+            endcase
         end
     end
+    
 endmodule
